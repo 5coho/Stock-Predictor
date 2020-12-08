389f555c72b2c129fecc5b5f15a148894744b65b
Scott Howes <howes_sm@hotmail.com>
2020-12-08 09:55:37 -0800

    Added build.sh for creating executable

---
+++ src/build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#This shell script will creat an executable for our project
+#Ensure all python dependencies are install before running this script
+#------NOTE------
+#For some reason, numpy version 1.19.4 does not work well with pyinstaller
+#only version 1.19.3 will work.
+
+#creating executable
+"pyinstaller" --onefile -i "media/chart.ico" --name="Stock Predictor" --noconsole main.py
+
+#copying Logical Clock executable to project root
+cp "dist/Stock Predictor" .
+
+#removing Logical Clock.spec file created by pyinstaller
+rm "Stock Predictor.spec"
+
+#removing build folder created by pyinstaller
+rm -r build
+
+#removing dist folder created by pyinstaller
+rm -r dist
